e29e61283224e5a082e8f46d4079e4579b57d321
ajg7 <ajgebara@gmail.com>
2020-09-07 23:27:18 -0500

    rendered items to the screen

--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -4,11 +4,15 @@ import { reducer, initialState } from "../reducers/reducer";
 
 const TodoList = props => {
     const [ state, dispatch ] = useReducer(reducer, initialState);
-    const [ todoItem, setTodoItem ] = useState("");
+
 
     return(
         <div>
-        
+            {state.map(todoItem => {
+                return(
+                    <p>{todoItem.item}</p>
+                )
+            })}
         </div>
     )
 }
