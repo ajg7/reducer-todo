038e7f2f726e119434a5ba952bb921aa432146f6
ajg7 <ajgebara@gmail.com>
2020-09-08 13:06:09 -0500

    able to add items to the screen

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import {TodoListForm} from './components/TodoListForm';
 import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="App">
-      <TodoListForm />
       <TodoList />
     </div>
   );
--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -7,8 +7,32 @@ const TodoList = props => {
     const [ todoItem, setTodoItem ] = useState("");
 
 
+    const handleChanges = event => {
+        console.log(event.target.value);
+        setTodoItem(event.target.value);
+    }
+
+    const submit = event => {
+        event.preventDefault();
+    }
+
+
     return(
         <>
+            <div>
+                <form onSubmit={submit}>
+                    <label>
+                        <input 
+                        type="text"
+                        name="input"
+                        className="todo-item-input"
+                        onChange={handleChanges}
+                        />
+                    </label>
+                    <button onClick={() => dispatch({type: "ADD_ITEM", payload: todoItem})}>Add Item</button>
+                </form>
+            </div>
+
             <div>
                 {state.map(todoItem => {
                     return(
